0be67fd777f13c8bd91fb2bf0a5a25e1b5ab6690
daniel <ghub@limitedcreativity.org>
2008-04-17 14:00:39 +0000

    Completed Hyper import functionality for all types except authors.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/trunk/talia_core@605 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- test/talia_util/path_import_test.rb
+++ test/talia_util/path_import_test.rb
@@ -11,7 +11,7 @@ require 'talia_util'
 module TaliaUtil
 
   # Test te DataRecord storage class
-  class FacsimileImportTest < Test::Unit::TestCase
+  class PathImportTest < Test::Unit::TestCase
   
     include UtilTestMethods
     
@@ -27,7 +27,7 @@ module TaliaUtil
         TaliaCore::TestHelper.flush_db
       end
       setup_once(:src) do
-        HyperImporter::Importer.import(load_doc('egrepalysviola-3259'))
+        HyperImporter::Importer.import(load_doc('igerike-907'))
       end
     end
     
@@ -38,22 +38,22 @@ module TaliaUtil
     
     # Test if the types were imported correctly
     def test_types
-      assert_types(@src, N::HYPER + "Facsimile", N::HYPER + "Color")
+      assert_types(@src, N::HYPER + "Path", N::HYPER + "Genetic")
     end
     
     # Test the title property
     def test_title
-      assert_property(@src.dcns::title)
+      assert_property(@src.dcns::title, "WS-25: Der Tausch und die Billigkeit")
     end
     
     # Test source name
     def test_siglum
-      assert_equal(N::LOCAL + "egrepalysviola-3259", @src.uri)
+      assert_equal(N::LOCAL + "igerike-907", @src.uri)
     end
     
     # Test the publishing date
     def test_pubdate
-      assert_property(@src.dcns::date, "2003-06-24")
+      assert_property(@src.dcns::date, "2005-01-05")
     end
     
     # Test the publisher
@@ -63,29 +63,22 @@ module TaliaUtil
 
     # Test if the curator was imported correctly
     def test_author
-      assert_property(@src.hyper::author, N::LOCAL::sviola, N::LOCAL::egrepaly)
+      assert_property(@src.hyper::author, N::LOCAL::igerike)
     end
-    
-    # Test if the data file was imported
-    def test_data
-      assert_equal(1, @src.data_records.size)
-      assert_kind_of(TaliaCore::ImageData, @src.data_records[0])
-      assert_equal('N-V-4,97.jpeg', @src.data_records[0].location)
-    end
-   
+
     # And now: already_published
     def test_already_published
       assert_property(@src.hyper::already_published, "no")
     end
     
-    # Test import of the dimensions
-    def test_dimensions
-      assert_property(@src.hyper::width, "2556")
-      assert_property(@src.hyper::height, "3988")
+    # Test the language setting
+    def test_language
+      assert_property(@src.dcns::language, "de")
     end
     
-    def test_units
-      assert_property(@src.hyper::dimension_units, 'pixel')
+    def test_description
+      # Useless test, but we found no path to test this with
+      assert_equal(0, @src.dcns::description.size)
     end
     
   end

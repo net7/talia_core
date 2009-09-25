4b1e672f7fa5fd45fa9df9108c672316f55520a0
Daniel Hahn <ghub@limitedcreativity.org>
2009-09-25 11:03:47 +0200

    Data directory for tests removed, will now be created and cleaned automatically without messing up the repo

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $: << File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib')
+require 'fileutils'
 require 'test/unit'
 require 'core_ext'
 require "talia_core"
@@ -26,6 +27,27 @@ module TaliaCore
       end
     end
     
+    def self.create_data_dir
+      ddir = TaliaCore::CONFIG['data_directory_location']
+      raise(RuntimeError, "No data directory configured") unless(ddir)
+      
+      if(File.exist?(ddir))
+        raise(RuntimeError, "Data dir not a directory") unless(File.directory?(ddir))
+        if(File.exist?(File.join(ddir, 'README_FOR_TEST')))
+          puts "*** REMOVING data directory at #{ddir} - this should be used for testing only!"
+          FileUtils.rm_rf(ddir)
+        else
+          puts "*** WARNING: The #{ddir} directory does not seem to be an automatic test directory."
+          puts "             Maybe something is wrong with your test/config/talia.yml ?"
+        end
+      end
+      unless(File.exist?(ddir))
+        fixture_dir = File.join(Test::Unit::TestCase.fixture_path, 'data_for_test')
+        puts "Copying fixture data to data directory #{ddir} -> #{fixture_dir}"
+        FileUtils.cp_r(fixture_dir, ddir)
+      end
+    end
+    
   end
   
   TestHelper.startup
@@ -35,5 +57,6 @@ module TaliaCore
     :semantic_relations => TaliaCore::SemanticRelation,
     :sources => TaliaCore::Source,
     :data_records => TaliaCore::DataTypes::DataRecord
+  TestHelper.create_data_dir
 
 end

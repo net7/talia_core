bbb5144ce06ceb69f5bec3cff05f41639609e5b7
Daniel Hahn <ghub@limitedcreativity.org>
2010-06-24 14:41:33 +0200

    More documentation stuff

--- lib/talia_core/data_types/mime_mapping.rb
+++ lib/talia_core/data_types/mime_mapping.rb
@@ -1,21 +1,48 @@
 module TaliaCore
   module DataTypes
 
-    # Mapping from Mime types to data classes and importing methods. Currently uses a fixed 
-    # default mapping. If the mime type is not known, it will use a fallback default handler.
+    # Mapping from Mime types to data classes and importing methods for DataRecord.
     #
-    # The mapping can be configured in Rails' initializer files. Example:
+    # See the DataTypes::DataLoader module to see how the import works. In a nutshell,
+    # each MIME type can be connected either to a DataTypes::FileRecord type that will
+    # be used for new data records, or the MIME type can be connected to a handler 
+    # method that will do the creation.
     #
-    #   TaliaCore::DataTypes::MimeMapping(:tiff, :image_data, :create_iip)
+    # = Default Mappings
+    #
+    # See the source code of the mapping_hash method for the detailed default mapping.
+    # It goes something like this: 
+    #
+    # * All image types (:jpeg, :tiff, :png, :gif, ...) use DataTypes::ImageData
+    # * HTML, XML and all "transcription" types (:html, :xml, :tei, ...) use DataTypes::XmlData
+    # * :text uses DataTypes::SimpleText
+    # * :pdf uses DataTypes::PdfData
+    # * The default (for unknow types) is to use DataTypes::FileRecord
+    #
+    # = Configure the MIME mappings for Talia
+    # 
+    # The mapping can be configured in Rails' initializer files (e.g. 
+    # config/initializers/talia.rb):
+    #
+    #   TaliaCore::DataTypes::MimeMapping.add_mapping(:tiff, :image_data, :create_iip)
+    #
+    # Add a mapping for each MIME type that you need, or where you want to change
+    # the default mapping
     class MimeMapping
 
       class << self
 
-        # Gets the data class for the given mime type
+        # Gets the data class for the given mime type. For loaders configured
+        # through add_mapping, this will always return the class corresponding
+        # to data_class. (Otherwise it will return the data_class configured 
+        # in the default mapping)
         def class_type_from(mime_type)
           mapping_for(mime_type)[:type]
         end
 
+        # Return the "loader type" for the given MIME type. This will return the 
+        # handler (as a symbol, see add_mapping) if set.
+        # If no handler is set, it will return the data_class (as class_type_from). 
         def loader_type_from(mime_type)
           map = mapping_for(mime_type)
           map[:loader] || map[:type]
@@ -23,23 +50,17 @@ module TaliaCore
         
         # Set a new mapping for the given MIME type. If only a class is given, this
         # will use the class to create new data records from. If a symbol is given
-        # for the class name, this will take the corresponding class from
+        # for data_class, this will take the corresponding class from
         # TaliaCore::DataTypes.
         #
-        # The loader method can be a symbol, if given it must correspond to a *class* 
-        # method that can be called on type_class and which accepts exactly 4 parameters,
-        # which will be passed in during record creation
-        # 
-        # * Mime type of the record 
-        # * 'location' field of the record
-        # * The record source - this is a descriptive string with either the url
-        #   or the file name from which the data should be fetched
-        # * The is_file flag. This will be true if the source is the name of a
-        #   regular file. Otherwise, the source field should be interpreted as
-        #   a URL.
-        # == Example handler method
-        #
-        #   def data_loader(mime_type, location, source, is_file)
+        # = Examples
+        #  
+        #  # Uses DataTypes::ImageData#create_iip to create new records
+        #  TaliaCore::DataTypes::MimeMapping.add_mapping(:tiff, :image_data, :create_iip)
+        #  
+        #  # Use the DataTypes::ImageData class for new data records, and create records
+        #  # in the default way (using create_with_file or similar)
+        #  TaliaCore::DataTypes::MimeMapping.add_mapping(:png, DataTypes::ImageData)
         def add_mapping(mime_type, data_class, handler = nil)
           mapping = {}
           if(!data_class.is_a?(Class))
@@ -54,23 +75,8 @@ module TaliaCore
           true
         end
         
-        private
-
-        def symbol_for(mime_type)
-          mime_type = Mime::Type.lookup(mime_type) unless(mime_type.is_a?(Mime::Type))
-          mime_type.to_sym
-        end
-
-        def mapping_for(mime_type)
-          mapping = mapping_hash[symbol_for(mime_type)]
-          TaliaCore.logger.warn { "No data class registered for mime type #{mime_type.inspect}, trying default handler." } unless(mapping)
-          mapping ||= mapping_hash[:default]
-
-          raise(ArgumentError, "No data class registered for type #{mime_type.inspect}") unless(mapping)
-          mapping
-        end
-
-        # Currently there is only the default mapping
+        # Returns the current mapping. This will be automatically initialized with
+        # the default mappings.
         def mapping_hash
           @mapping ||= {
             :xml => { :type => DataTypes::XmlData },
@@ -91,6 +97,27 @@ module TaliaCore
             # Default fallback handler
             :default => { :type => FileRecord }
           }
+        end
+        
+        private
+        
+        # Returns the symbol that corresponds to a given MIME type. The MIME type
+        # can be a Mime::Type object, a MIME string like 'text/html' or a MIME
+        # symbol like :jpeg
+        def symbol_for(mime_type)
+          mime_type = Mime::Type.lookup(mime_type) unless(mime_type.is_a?(Mime::Type))
+          mime_type.to_sym
+        end
+
+        # Return the mapping for the given MIME type. If no mapping is defined for the
+        # MIME type, it will log a warning and use the "default" mapping.
+        def mapping_for(mime_type)
+          mapping = mapping_hash[symbol_for(mime_type)]
+          TaliaCore.logger.warn { "No data class registered for mime type #{mime_type.inspect}, trying default handler." } unless(mapping)
+          mapping ||= mapping_hash[:default]
+
+          raise(ArgumentError, "No data class registered for type #{mime_type.inspect}") unless(mapping)
+          mapping
         end
 
       end
